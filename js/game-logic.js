05cf352ad3edaae2b7eaeb3181982c5929c8eeac
SoniaCWeb <sonia.choudhury@hotmail.co.uk>
2020-11-17 15:14:29 +0000

    variables tests passed

--- js/game-logic.js
+++ js/game-logic.js
@@ -1,1 +1,14 @@
 // All code should be written in this file.
+
+//global variables
+let playerOneMoveOneType
+let playerOneMoveTwoType
+let playerOneMoveThreeType
+let playerTwoMoveOneType
+let playerTwoMoveTwoType
+let playerOneMoveOneValue
+let playerOneMoveTwoValue
+let playerOneMoveThreeValue
+let playerTwoMoveOneValue
+let playerTwoMoveTwoValue
+let playerTwoMoveThreeValue
