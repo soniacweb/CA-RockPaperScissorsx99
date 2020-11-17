874c947d70dc5911894d0f657f39c3fba9084fb5
SoniaCWeb <sonia.choudhury@hotmail.co.uk>
2020-11-17 21:01:23 +0000

    18 tests passing

--- js/game-logic.js
+++ js/game-logic.js
@@ -1,14 +1,75 @@
 // All code should be written in this file.
 
 //global variables
-let playerOneMoveOneType
-let playerOneMoveTwoType
-let playerOneMoveThreeType
-let playerTwoMoveOneType
-let playerTwoMoveTwoType
-let playerOneMoveOneValue
-let playerOneMoveTwoValue
-let playerOneMoveThreeValue
-let playerTwoMoveOneValue
-let playerTwoMoveTwoValue
-let playerTwoMoveThreeValue
+let playerOneMoveOneType,
+    playerOneMoveTwoType,
+    playerOneMoveThreeType,
+    playerTwoMoveOneType,
+    playerTwoMoveTwoType,
+    playerOneMoveOneValue,
+    playerOneMoveTwoValue,
+    playerOneMoveThreeValue,
+    playerTwoMoveOneValue,
+    playerTwoMoveTwoValue,
+    playerTwoMoveThreeValue
+
+//function called setPlayerMoves() for main logic
+
+const setPlayerMoves = (player, moveOneType, moveOneValue, moveTwoType, moveTwoValue, moveThreeType, moveThreeValue) => {
+  if (!player || !moveOneType || !moveOneValue || !moveTwoType || !moveTwoValue || !moveThreeValue || !moveThreeType) {
+    return;
+  }
+  else if (!isValidMoveType(moveOneType) || !isValidMoveType(moveTwoType) || !isValidMoveType(moveThreeType)) {
+    return;
+  }
+  else if (!isValidMoveValue(moveOneValue) || !isValidMoveValue(moveTwoValue) || !isValidMoveValue(moveThreeValue)) {
+  return;
+  }
+  else if ((moveOneValue + moveTwoValue + moveThreeValue) > 99) {
+    return
+  }
+  else if (player === 'Player One') {
+    playerOneMoveOneType = moveOneType;
+    playerOneMoveOneValue = moveOneValue;
+    playerOneMoveTwoType = moveTwoType;
+    playerOneMoveTwoValue = moveTwoValue;
+    playerOneMoveThreeType = moveThreeType;
+    playerOneMoveThreeValue = moveThreeValue;
+  } else if (player === 'Player Two') {
+    playerTwoMoveOneType = moveOneType;
+    playerTwoMoveOneValue = moveOneValue;
+    playerTwoMoveTwoType = moveTwoType;
+    playerTwoMoveTwoValue = moveTwoValue;
+    playerTwoMoveThreeType = moveThreeType;
+    playerTwoMoveThreeValue = moveThreeValue;
+  }
+}
+
+//helper functions
+const isValidmoveType = (move) => {
+  return (move === 'rock') || (move === 'paper') || (move === 'scissors') 
+}
+
+const isValidMoveValue = (moveValue) => {
+  return (moveValue >= 1) && (moveValue <= 99 )
+}
+
+function getRoundWinner(player, moveOneType, moveOneValue, moveTwoType, moveTwoValue, moveThreeType, moveThreeValue) {
+
+ }
+
+function getRoundWinner() {
+  // A function called getRoundWinner, which takes a round number (1, 2, or 3), 
+  // compares both player’s move types and values for that 
+  // round, and returns the appropriate winner ('Player One', 'Player Two', or 'Tie')
+}
+
+function getGameWinner() {
+  // which compares both player’s move types and values for the whole game and returns
+  // the appropriate winner ('Player One', 'Player Two', or 'Tie')
+}
+
+function setComputerMoves() {
+//  let randomMove =  Math.floor(Match.random() * 3 + 1)
+
+}
