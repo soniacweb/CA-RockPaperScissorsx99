0e77c9c1cf7aa4f43548eef5e89a52dc2b0f2dd8
SoniaCWeb <sonia.choudhury@hotmail.co.uk>
2020-11-17 21:27:33 +0000

    42 passing

--- js/game-logic.js
+++ js/game-logic.js
@@ -2,74 +2,170 @@
 
 //global variables
 let playerOneMoveOneType,
-    playerOneMoveTwoType,
-    playerOneMoveThreeType,
-    playerTwoMoveOneType,
-    playerTwoMoveTwoType,
-    playerOneMoveOneValue,
-    playerOneMoveTwoValue,
-    playerOneMoveThreeValue,
-    playerTwoMoveOneValue,
-    playerTwoMoveTwoValue,
-    playerTwoMoveThreeValue
+  playerOneMoveTwoType,
+  playerOneMoveThreeType,
+  playerTwoMoveOneType,
+  playerTwoMoveTwoType,
+  playerOneMoveOneValue,
+  playerOneMoveTwoValue,
+  playerOneMoveThreeValue,
+  playerTwoMoveOneValue,
+  playerTwoMoveTwoValue,
+  playerTwoMoveThreeValue,
+  playerOneWins,
+  playerTwoWins
 
 //function called setPlayerMoves() for main logic
 
-const setPlayerMoves = (player, moveOneType, moveOneValue, moveTwoType, moveTwoValue, moveThreeType, moveThreeValue) => {
+function setPlayerMoves(player, moveOneType, moveOneValue, moveTwoType, moveTwoValue, moveThreeType, moveThreeValue) {
   if (!player || !moveOneType || !moveOneValue || !moveTwoType || !moveTwoValue || !moveThreeValue || !moveThreeType) {
-    return;
-  }
-  else if (!isValidMoveType(moveOneType) || !isValidMoveType(moveTwoType) || !isValidMoveType(moveThreeType)) {
-    return;
-  }
-  else if (!isValidMoveValue(moveOneValue) || !isValidMoveValue(moveTwoValue) || !isValidMoveValue(moveThreeValue)) {
-  return;
-  }
-  else if ((moveOneValue + moveTwoValue + moveThreeValue) > 99) {
     return
   }
-  else if (player === 'Player One') {
-    playerOneMoveOneType = moveOneType;
-    playerOneMoveOneValue = moveOneValue;
-    playerOneMoveTwoType = moveTwoType;
-    playerOneMoveTwoValue = moveTwoValue;
-    playerOneMoveThreeType = moveThreeType;
-    playerOneMoveThreeValue = moveThreeValue;
+  if (!isValidMoveType(moveOneType) || !isValidMoveType(moveTwoType) || !isValidMoveType(moveThreeType)) {
+    return
+  }
+  if (!isValidMoveValue(moveOneValue) || !isValidMoveValue(moveTwoValue) || !isValidMoveValue(moveThreeValue)) {
+    return
+  }
+  if ((moveOneValue + moveTwoValue + moveThreeValue) > 99) {
+    return
+  }
+  if (player === 'Player One') {
+    playerOneMoveOneType = moveOneType
+    playerOneMoveOneValue = moveOneValue
+    playerOneMoveTwoType = moveTwoType
+    playerOneMoveTwoValue = moveTwoValue
+    playerOneMoveThreeType = moveThreeType
+    playerOneMoveThreeValue = moveThreeValue
   } else if (player === 'Player Two') {
-    playerTwoMoveOneType = moveOneType;
-    playerTwoMoveOneValue = moveOneValue;
-    playerTwoMoveTwoType = moveTwoType;
-    playerTwoMoveTwoValue = moveTwoValue;
-    playerTwoMoveThreeType = moveThreeType;
-    playerTwoMoveThreeValue = moveThreeValue;
+    playerTwoMoveOneType = moveOneType
+    playerTwoMoveOneValue = moveOneValue
+    playerTwoMoveTwoType = moveTwoType
+    playerTwoMoveTwoValue = moveTwoValue
+    playerTwoMoveThreeType = moveThreeType
+    playerTwoMoveThreeValue = moveThreeValue
   }
 }
 
 //helper functions
-const isValidmoveType = (move) => {
+function isValidMoveType(move) {
   return (move === 'rock') || (move === 'paper') || (move === 'scissors') 
 }
 
-const isValidMoveValue = (moveValue) => {
-  return (moveValue >= 1) && (moveValue <= 99 )
+function isValidMoveValue(moveValue) {
+  return (moveValue >= 1) && (moveValue <= 99)
 }
 
-function getRoundWinner(player, moveOneType, moveOneValue, moveTwoType, moveTwoValue, moveThreeType, moveThreeValue) {
 
- }
-
-function getRoundWinner() {
+function getRoundWinner(roundNum) {
   // A function called getRoundWinner, which takes a round number (1, 2, or 3), 
   // compares both player’s move types and values for that 
   // round, and returns the appropriate winner ('Player One', 'Player Two', or 'Tie')
+  switch (roundNum) {
+    case 1:
+      return getMoveWinner(playerOneMoveOneType,
+        playerOneMoveOneValue,
+        playerTwoMoveOneType,
+        playerTwoMoveOneValue)
+    case 2:
+      return getMoveWinner(playerOneMoveTwoType,
+        playerOneMoveTwoValue,
+        playerTwoMoveTwoType,
+        playerTwoMoveTwoValue)
+    case 3:
+      return getMoveWinner(playerOneMoveThreeType,
+        playerOneMoveThreeValue,
+        playerTwoMoveThreeType,
+        playerTwoMoveThreeValue)
+    default:
+      return null
+  }
+}
+
+function getMoveWinner(playerOneMoveType, playerOneMoveValue, playerTwoMoveType,
+  playerTwoMoveValue) {
+  if (!playerOneMoveType || !playerOneMoveValue || !playerTwoMoveType ||
+      !playerTwoMoveValue) {
+    return null
+  }
+
+  if (playerOneMoveType === playerTwoMoveType) {
+    if (playerOneMoveValue > playerTwoMoveValue) {
+      return 'Player One'
+    } else if (playerOneMoveValue < playerTwoMoveValue) {
+      return 'Player Two'
+    } else {
+      return 'Tie'
+    }
+  }
+  if (playerOneMoveType === 'rock') {
+    if (playerTwoMoveType === 'scissors') {
+      return 'Player One'
+    } else {
+      return 'Player Two'
+    }
+  } else if (playerOneMoveType === 'paper') {
+    if (playerTwoMoveType === 'rock') {
+      return 'Player One'
+    } else {
+      return 'Player Two'
+    }
+  } else {
+    if (playerTwoMoveType === 'paper') {
+      return 'Player One'
+    } else {
+      return 'Player Two'
+    }
+  }
 }
 
 function getGameWinner() {
   // which compares both player’s move types and values for the whole game and returns
   // the appropriate winner ('Player One', 'Player Two', or 'Tie')
+  if (!playerOneMoveOneType || !playerOneMoveTwoType ||
+      !playerOneMoveThreeType || !playerOneMoveOneValue ||
+      !playerOneMoveTwoValue || !playerOneMoveThreeValue ||
+      !playerTwoMoveOneType || !playerTwoMoveTwoType ||
+      !playerTwoMoveThreeType || !playerTwoMoveOneValue ||
+      !playerTwoMoveTwoValue || !playerTwoMoveThreeValue) {
+    return null
+  }
+  playerOneWins = 0
+  playerTwoWins = 0
+
+  const roundOneWinner = getRoundWinner(1)
+  const roundTwoWinner = getRoundWinner(2)
+  const roundThreeWinner = getRoundWinner(3)
+
+  addWin(roundOneWinner)
+  addWin(roundTwoWinner)
+  addWin(roundThreeWinner)
+  if (playerOneWins > playerTwoWins) {
+    return 'Player One'
+  } else if (playerOneWins < playerTwoWins) {
+    return 'Player Two'
+  } else {
+    return 'Tie'
+  }
+}
+
+function addWin(winner) {
+  if (winner === 'Player One') {
+    playerOneWins = (playerOneWins + 1) || 1
+  } else if (winner === 'Player Two') {
+    playerTwoWins = (playerTwoWins + 1) || 1
+  }
 }
 
 function setComputerMoves() {
-//  let randomMove =  Math.floor(Match.random() * 3 + 1)
-
+  const moves = ['rock', 'paper', 'scissors']
+  const moveOneType = moves[Math.floor(Math.random() * 3)]
+  const moveTwoType = moves[Math.floor(Math.random() * 3)]
+  const moveThreeType = moves[Math.floor(Math.random() * 3)]
+  const moveOneValue = Math.floor(Math.random() * 96) + 1
+  const moveTwoValue = Math.floor(Math.random() * (97 - moveOneValue)) + 1
+  const moveThreeValue = 99 - moveOneValue - moveTwoValue
+  setPlayerMoves('Player Two', moveOneType, moveOneValue, moveTwoType,
+    moveTwoValue, moveThreeType, moveThreeValue)
 }
+
